004332c445ae75bba7166bd2d00079dbb56e7f16
Vinnydaninja <vincenguyen35@gmail.com>
2022-12-29 21:56:43 -0800

    Fix error code for CREATE controller

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -28,7 +28,7 @@ const createWorkout = async (req, res) => {
         const workout = await Workout.create({ title, reps, load });
         res.status(200).json(workout);
     } catch(err) {
-        res.status(404).json({ error: err.message });
+        res.status(400).json({ error: err.message });
     }
 }
 
